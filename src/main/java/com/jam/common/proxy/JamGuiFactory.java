2f487befa7fb52ea973588b691c43166907e9e05
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-08-08 00:07:45 +0100

    Only real changes needed for 1.12

--- src/main/java/com/jam/common/proxy/JamGuiFactory.java
+++ src/main/java/com/jam/common/proxy/JamGuiFactory.java
@@ -27,17 +27,7 @@ public class JamGuiFactory implements IModGuiFactory {
     }
 
     @Override
-    public Class<? extends GuiScreen> mainConfigGuiClass() {
-        return JamGuiConfig.class;
-    }
-
-    @Override
     public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
-        return null;
-    }
-
-    @Override
-    public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
         return null;
     }
 }
