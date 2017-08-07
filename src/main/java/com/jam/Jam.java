7cbe3b344d015574af9c808bd9be09210ee86936
Michael Hillcox <hello.michaelhillcox@gmail.com>
2017-08-07 17:04:24 +0100

    Switched for the correct handlers

--- src/main/java/com/jam/Jam.java
+++ src/main/java/com/jam/Jam.java
@@ -3,11 +3,12 @@ package com.jam;
 import com.jam.client.ClientProxy;
 import com.jam.common.Ref;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * Created by MiKeY on 07/08/17.
@@ -18,23 +19,23 @@ public class Jam {
 
     public static Boolean jamEnabled = false;
 
-    @Mod.Instance(Ref.MOD_ID)
+    @Instance(Ref.MOD_ID)
     public static Jam instance;
 
     @SidedProxy(clientSide="com.jam.client.ClientProxy")
     private static ClientProxy proxy;
 
-    @SubscribeEvent
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         System.out.println("Jam Pre Init");
     }
 
-    @SubscribeEvent
+    @EventHandler
     public void init(FMLInitializationEvent event) {
 
     }
 
-    @SubscribeEvent
+    @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
 
     }
