dbd92b4973c540d5b74980435393a5cc7707c7a7
Michael Hillcox <hello.michaelhillcox@gmail.com>
2018-02-14 20:11:04 +0000

    Removed chat strings

--- src/main/java/com/jam/client/KeyBindingHandler.java
+++ src/main/java/com/jam/client/KeyBindingHandler.java
@@ -1,13 +1,11 @@
 package com.jam.client;
 
-import com.jam.common.Jam;
 import com.jam.common.lib.Ref;
 import com.jam.common.server.JamPacketHandler;
 import com.jam.common.server.Packet;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -34,10 +32,6 @@ public class KeyBindingHandler {
     {
         if(toggleJam.isPressed()) {
             JamPacketHandler.NetworkInstance.sendToServer(new Packet());
-            if( Jam.jamEnabled )
-                mc.ingameGUI.getChatGUI().printChatMessage( new TextComponentString(I18n.format("jam.toggle.enabled")));
-            else
-                mc.ingameGUI.getChatGUI().printChatMessage( new TextComponentString(I18n.format("jam.toggle.disabled")));
         }
     }
 }
