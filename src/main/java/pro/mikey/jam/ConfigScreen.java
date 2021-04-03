c84cd7a273fb1d0e085675ca56aaf80c3aa7a22a
Michael Hillcox <michaelhillcox@gmail.com>
2021-04-03 16:19:00 +0100

    fix: cleaned up gui

--- src/main/java/pro/mikey/jam/ConfigScreen.java
+++ src/main/java/pro/mikey/jam/ConfigScreen.java
@@ -26,12 +26,14 @@ public class ConfigScreen extends Screen {
         this.range = playerTag.getInt(JamNbtKeys.RANGE);
         this.speed = playerTag.getDouble(JamNbtKeys.SPEED);
 
-        this.addButton(new Button(0, 0, 200, 20, new TranslationTextComponent("jam.screen.teleport", this.teleport).getFormattedText(), button -> {
+        int x = this.width / 2, y = this.height / 2;
+
+        this.addButton(new Button(x - 100, y - 35, 200, 20, new TranslationTextComponent("jam.screen.teleport", this.teleport).getFormattedText(), button -> {
             this.teleport = !this.teleport;
             button.setMessage(new TranslationTextComponent("jam.screen.teleport", this.teleport).getFormattedText());
         }));
 
-        AbstractSlider rangeSlider = new AbstractSlider(0, 40, 200, 20, this.range / 100f) {
+        AbstractSlider rangeSlider = new AbstractSlider(x - 100, y - 10, 200, 20, this.range / 100f) {
             @Override
             protected void updateMessage() {
                 this.setMessage(new TranslationTextComponent("jam.screen.range", (int) (this.value * 100)).getFormattedText());
@@ -45,7 +47,7 @@ public class ConfigScreen extends Screen {
         rangeSlider.setMessage(new TranslationTextComponent("jam.screen.range", this.range).getFormattedText());
         this.addButton(rangeSlider);
 
-        AbstractSlider speedSlider = new AbstractSlider(0, 80, 200, 20, this.speed) {
+        AbstractSlider speedSlider = new AbstractSlider(x - 100, y + 15, 200, 20, this.speed) {
             @Override
             protected void updateMessage() {
                 this.setMessage(new TranslationTextComponent("jam.screen.speed", ((int) (this.value * 100) / 100f)).getFormattedText());
@@ -64,6 +66,13 @@ public class ConfigScreen extends Screen {
     public void render(int mouseX, int mouseY, float partialTicks) {
         this.renderBackground();
         super.render(mouseX, mouseY, partialTicks);
+
+        this.drawCenteredString(this.getMinecraft().fontRenderer, new TranslationTextComponent("jam.mod_name").getString(), this.width / 2, this.height / 2 - 60, 0xFFFFFF);
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
     }
 
     @Override
